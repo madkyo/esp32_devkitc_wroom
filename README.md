e34ad840c58415f9fd4e5adece8c6664c9b8503d
ben chen <ben.chen2@quanta.corp-partner.google.com>
2026-03-16 16:03:51 +0800

    esp-32s: button interrupt
    Using button start LED zephyr dts spec control
    
    ref: github bokfink Channel part 3

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # esp32_devkitc_wroom
-# esp32_devkitc_wroom
+
 Note:
 > This folder only contains ESP modified sample Firmware source files.
 > For ESP32 Firmware code - users should get it directly this to build quickly
 
+
+1. source ~/zephyrproject/.venv/bin/activate
+
+2. west build -p always -b esp32_devkitc_wroom/esp32/procpu
+
+3. west flash
+
+4. minicom -D /dev/ttyUSB0
+(.venv$: west espressif monitor -p /dev/ttyUSB0)
+
+=========================================================================
+
 HW : NodeMCU-32
-[board Specification](https://uamper.com/products/datasheet/NodeMCU32-S_specification_V1.3.pdf?srsltid=AfmBOoo-2bG2DRyNEk61-R3R9b4ghIr9yflIHDpdr4ETYT5WFWgLW5HZ)
-===============================================================================
+
+Reference:
+
+1. [esp-32s: button interrupt sample log:](https://drive.google.com/file/d/1ZcRvYyaoXPmLGt05yoVfGqjqnXD71D_m/view?usp=drive_link)
+
+2. [board Specification NodeMCU-32 Ai-Thinker linker](https://uamper.com/products/datasheet/NodeMCU32-S_specification_V1.3.pdf?srsltid=AfmBOoo-2bG2DRyNEk61-R3R9b4ghIr9yflIHDpdr4ETYT5WFWgLW5HZ)
+
+3. [zephyr 3.7.0 esp32_devkitc_wroom](https://docs.zephyrproject.org/3.7.0/boards/espressif/esp32_devkitc_wroom/doc/index.html)
+
+4. [ADUINO SAMPLES WEB](https://www.lanmootech.com/courses/ultimate/lesson/%e9%96%8b%e9%97%9c%e6%8e%a7%e5%88%b6-led-%e7%87%88-%e5%96%ae%e5%85%83/)
+
+youtube:
+
+5. [ESP32 on Zephyr OS: GPIO Interrupt (ISR) [Part 3]](https://www.youtube.com/watch?v=NlRM2IfODTs)
--- src/main.c
+++ src/main.c
@@ -2,27 +2,87 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
+#define BUILD_STATE "bring"
+#define LED_ON_OFFSEC_TEST 0
+
+static struct gpio_callback button_cb_data;
+
 static const struct gpio_dt_spec led =
   GPIO_DT_SPEC_GET(DT_NODELABEL(blinking_led), gpios);
 
+static const struct gpio_dt_spec button =
+  GPIO_DT_SPEC_GET(DT_NODELABEL(button), gpios);
+
+void button_pressed(const struct device *dev,
+        struct gpio_callback *cb,
+        uint32_t pins)
+{
+  int ret;
+  int state;
+
+  state = gpio_pin_get_dt(&button);
+
+  printk("%s: %d", __func__, state);
+
+  if (state) {
+    ret = gpio_pin_toggle_dt(&led);
+    printk("toggle ok\n");
+    if (ret != 0) {
+      printk("Could not toggle LED\n");
+    }
+  } else {
+    printk("end \n");
+  }
+}
+
 int main(void)
 {
+  int ret, irq_key;
+
+  printk("Hi %s state %s Up \n", CONFIG_SOC, BUILD_STATE);
+
   if (!device_is_ready(led.port)) {
     return 0;
   }
 
-  int ret;
+  if (!device_is_ready(button.port)) {
+    return 0;
+  }
+
   ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
   if (ret != 0) {
     return 0;
   }
 
+  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+  if (ret != 0) {
+    return 0;
+  }
+
+  ret = gpio_pin_interrupt_configure_dt(&button,
+          GPIO_INT_EDGE_BOTH);
+
+  if (ret != 0) {
+    return 0;
+  }
+
+  gpio_init_callback(&button_cb_data, button_pressed,
+         BIT(button.pin));
+  gpio_add_callback(button.port, &button_cb_data);
+
+  irq_key = irq_lock();
+  button_pressed(button.port, &button_cb_data, BIT(button.pin));
+  irq_unlock(irq_key);
+
+#if LED_ON_OFFSEC_TEST
   while (true) {
     ret = gpio_pin_toggle_dt(&led);
     if (ret != 0) {
       return 0;
     }
     k_msleep(1000);
-
+    printk(" LED toggle \n");
   }
+#endif
+
 }
